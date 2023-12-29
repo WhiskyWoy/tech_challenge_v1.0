84ea934286d6c5150f99a3728064e12e8234e637
JulianD2000 <julian.dormehl@gmail.com>
2023-12-29 12:14:16 +0100

    Going through the code and insert some comments for basic understanding

--- Start.py
+++ Start.py
@@ -1,16 +1,21 @@
+# Importing libraries
 import streamlit as st
 from pdfminer.high_level import extract_text
 import pandas as pd
 import backend
 
+# Configuring the default settings of the page
 st.set_page_config(
     page_title="Start Page",
     page_icon="👋",
     layout="wide",
 )
+
+# Ensuring the information storage across sessions
 if 'df' not in st.session_state:
     st.session_state.df = pd.DataFrame()
 
+# Formatting of the page
 st.write("# Welcome to Jasmin, your AI Judge Assistant! 👋")
 
 st.sidebar.write("Upload your document and then select a feature above.")
@@ -26,7 +31,7 @@ st.markdown(
 """
 )
 
-
+# Allowing users to upload multiple files and storing the text in a dictionary
 data_dict = {'filename': [], 'text': []}
 uploaded_files = st.file_uploader("Choose a Text file", accept_multiple_files=True)
 for uploaded_file in uploaded_files:
@@ -38,8 +43,7 @@ for uploaded_file in uploaded_files:
     data_dict['filename'].append(uploaded_file.name)
     data_dict['text'].append(text_data)
     
-
-
+# In the case of a successful upload passing the data dictionary to the backend
 uploaded = st.button('Upload', key='up')
 if uploaded and len(uploaded_files) > 0:
     st.success('Uploaded successfully, proccessing...')
