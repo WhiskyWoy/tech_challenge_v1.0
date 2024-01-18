4987bd6276702e2b95715c7943ed4c6d364c371f
Philipp <philippkutschker@gmail.com>
2024-01-18 17:44:54 +0100

    Updated feature 3, included version for dryrun

--- Start.py
+++ Start.py
@@ -30,6 +30,8 @@ st.markdown(
 """
 )
 
+st.markdown("Laden Sie zunächst den Schriftsatz des Klägers und den Schriftsatz des Beklagten hoch. Achten Sie darauf, die Schriftsätze in dem korrekten Feld hochzuladen.")
+st.text("")
 
 # Allowing users to upload multiple files
 upload_plaintiff = st.file_uploader("Kläger Schriftsatz")
@@ -61,5 +63,26 @@ if upload_plaintiff and upload_defendant:
         with open(path_defendant, "wb") as f_defendant:
             f_defendant.write(upload_defendant.getbuffer())
         backend.call(uploaded_files, backend.gpt_4)
-        st.success("Verarbeitung erfolgreich! Wähle eine Option aus der Sidebar aus.")
+        st.success("Verarbeitung erfolgreich!")
         st.sidebar.success("Wähle eine Option aus der Sidebar aus.")
+
+        st.text("")
+        st.text("")
+        st.text("")
+
+        # Display images in two columns
+        col1, col2, col3, col4, col5, col6, col7 = st.columns([1, 0.2, 1, 0.2, 1, 0.2, 1])
+
+        col1.image("pictures/Bild1.jpg", use_column_width=True)
+        col3.image("pictures/Bild2.jpg", use_column_width=True)
+        col5.image("pictures/Bild3.jpg", use_column_width=True)
+        col7.image("pictures/Bild4.jpg", use_column_width=True)
+
+        if col1.button("Zusammenfassung", use_container_width=True):
+            st.switch_page("pages/1_Zusammenfassung.py")
+        if col3.button("Tabelle an Fakten", use_container_width=True):
+            st.switch_page("pages/2_Tabelle.py")
+        if col5.button("Direkter Schriftsatz Vergleich", use_container_width=True):
+            st.switch_page("pages/3_Schriftsätze.py")
+        if col7.button("Chat mit Jasmin", use_container_width=True):
+            st.switch_page("pages/4_Jasmin.py")
